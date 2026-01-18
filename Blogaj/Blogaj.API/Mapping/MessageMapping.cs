13c030f5e485bde5e39d3441b9ad7b362895c93e
batuhann9 <batuhanozkanli9@gmail.com>
2026-01-18 23:54:52 +0300

    fix: finalize CRUD controllers and mappings

---
+++ Blogaj/Blogaj.API/Mapping/MessageMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Blogaj.DTO.DTOs.MessageDtos;
+using Blogaj.Entity.Entities;
+
+namespace Blogaj.API.Mapping
+{
+    public class MessageMapping:Profile
+    {
+        public MessageMapping()
+        {
+            CreateMap<CreateMessageDto, Message>().ReverseMap();
+            CreateMap<UpdateMessageDto, Message>().ReverseMap();
+
+        }
+    }
+}
